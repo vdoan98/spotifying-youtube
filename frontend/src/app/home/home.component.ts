9346de33458bfe13904f5fde0bc0a152112b79d6
Vy Doan <vdoan98@gmail.com>
2020-08-29 15:17:59 -0500

    feat: Add front page animation. TODO: onload trigger animation.

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    // Each unique animation requires its own trigger. The first argument of the trigger function is the name
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotate(0)' })),
+      state('rotated', style({ transform: 'rotate(-360deg)' })),
+      transition('rotated => default', animate('2000ms ease-out')),
+      transition('default => rotated', animate('2000ms ease-in'))
+  ])
+]
 })
 export class HomeComponent implements OnInit {
-
+  state: string = 'default';
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.rotate()
   }
+
+  rotate() {
+    this.state = (this.state === 'default' ? 'rotated' : 'default');
+  }
+
 
 }
