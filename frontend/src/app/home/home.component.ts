be80c4889cdb9207a0e0b45d139a5b7eb93d68c0
Vy Doan <vdoan98@gmail.com>
2020-08-29 15:48:25 -0500

    feat: Add requirements.txt.

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -8,10 +8,10 @@ import {trigger, state, style, animate, transition} from '@angular/animations';
   animations: [
     // Each unique animation requires its own trigger. The first argument of the trigger function is the name
     trigger('rotatedState', [
-      state('default', style({ transform: 'rotate(0)' })),
+      state('default', style({ transform: 'rotate(360deg)' })),
       state('rotated', style({ transform: 'rotate(-360deg)' })),
       transition('rotated => default', animate('2000ms ease-out')),
-      transition('default => rotated', animate('2000ms ease-in'))
+      transition('default => rotated', animate('2000ms ease-in')),
   ])
 ]
 })
@@ -20,12 +20,18 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.rotate()
   }
 
-  rotate() {
-    this.state = (this.state === 'default' ? 'rotated' : 'default');
+  rotateForth() {
+    //this.state = (this.state === 'default' ? 'rotated' : 'default');
+    this.state = 'default';
   }
+
+  rotateBack() {
+    this.state = 'rotated';
+  }
+
+
 
 
 }
